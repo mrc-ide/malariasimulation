070d4290e2a322effbede335927d24dfea5dea38
Giovanni Charles <giovanni.charles@gmail.com>
2020-07-21 11:58:37 +0100

    Remove obsolete state change process

--- R/processes.R
+++ R/processes.R
@@ -69,12 +69,6 @@ create_processes <- function(
       variables$infectivity,
       0
     ),
-    individual::fixed_probability_state_change_process(
-      individuals$human$name,
-      states$Tr$name,
-      states$S$name,
-      1. - exp(-1./parameters$dt)
-    ),
 
     # schedule infections for humans and set last_boosted_*
     create_infection_process(

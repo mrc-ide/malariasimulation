f106d98b6bd752dac7949857933a843cc70283b7
Giovanni Charles <giovanni.charles@gmail.com>
2020-10-05 16:33:17 +0100

    Improve documentation for vector control strategy

--- R/vector_control_parameters.R
+++ R/vector_control_parameters.R
@@ -1,7 +1,22 @@
 #' @title Parameterise a bed net strategy
+#'
+#' @description The model will distribute bednets at `timesteps` to a random
+#' sample of the entire human population. The sample size will be a proportion
+#' of the human population taken from the corresponding `coverages`.
+#' The sample _can_ contain humans who already have bednets.
+#'
+#' All of the sample "use" their bednets on the timestep after they are
+#' distributed. Incomplete usage is not part of this model.
+#'
+#' If a human in the sample already has a bednet, their bednet will be replaced
+#' by a new one.
+#'
+#' Bednets will be randomly removed each timestep with a rate of `1 -
+#' exp(-1/retention)`
+#'
 #' @param parameters a list of parameters to modify
 #' @param timesteps the timesteps at which to distribute bednets
-#' @param coverages the proportion of the target population who receive bednets
+#' @param coverages the proportion of the human population who receive bednets
 #' @param retention the average number of timesteps a net is kept for
 #' @export
 set_bednets <- function(
@@ -21,9 +36,18 @@ set_bednets <- function(
 }
 
 #' @title Parameterise an indoor spraying strategy
+#'
+#' @description The model will apply indoor spraying at `timesteps` to a random
+#' sample of the entire human population. The sample size will be a proportion
+#' of the human population taken from the corresponding `coverages`.
+#' The sample _can_ contain humans who have already benefitted from spraying.
+#'
+#' If a human in the sample lives in a sprayed house, the efficacy of the
+#' spraying will be returned to the maximum.
+#'
 #' @param parameters a list of parameters to modify
 #' @param timesteps the timesteps at which to spray
-#' @param coverages the proportion of the target population who get indoor
+#' @param coverages the proportion of the population who get indoor
 #' spraying
 #' @export
 set_spraying <- function(

9624b4e3bc0b97ea4b9eb7df40a62d0e877ab118
Giovanni Charles <giovanni.charles@gmail.com>
2020-10-12 11:33:58 +0100

    Implement correlation sampling

---
+++ R/correlation.R
@@ -0,0 +1,130 @@
+INTS <- c(
+  'rtss',
+  'mda',
+  'smc',
+  'tbv',
+  'bednets',
+  'spraying'
+)
+
+#' Class: Correlation parameters
+#' Describes an event in the simulation
+#' @export CorrelationParameters
+CorrelationParameters <- R6::R6Class(
+  'CorrelationParameters',
+  private = list(
+    interventions = list(),
+    n_ints = function() length(private$interventions),
+    population = 0,
+    rho_matrix = NULL,
+    rho = function() diag(private$rho_matrix),
+    .sigma = NULL,
+    .mvnorm = NULL
+  ),
+  public = list(
+
+    #' @description initialise correlation parameters
+    #' @param parameters model parameters
+    initialize = function(parameters) {
+      # Find a list of enabled interventions
+      enabled <- vlapply(INTS, function(name) parameters[[name]])
+      private$interventions <- INTS[enabled]
+
+      # Initialise a rho matrix for our interventions
+      n_ints <- private$n_ints()
+      private$rho_matrix <- matrix(
+        0,
+        nrow = n_ints,
+        ncol = n_ints,
+        dimnames = list(private$interventions, private$interventions)
+      )
+
+      # Store population for mvnorm draws
+      private$population <- parameters$human_population
+    },
+
+    #' @description Add rho between rounds
+    #' @param intervention string representing the intervention to update
+    #' @param rho value between 0 and 1 representing the correlation between rounds of
+    #' the intervention
+    inter_round_rho = function(int, rho) {
+      if (!(int %in% private$interventions)) {
+        stop(paste0('invalid intervention name: ', int))
+      }
+      if (rho < 0 || rho > 1) {
+        stop(paste0('rho for ', int, 'must be between 0 and 1'))
+      }
+      if (rho == 1) {
+        rho <- 1 - .Machine$double.eps
+      }
+      private$rho_matrix[[int, int]] <- rho
+      private$.sigma <- NULL
+    },
+
+    #' @description Add rho between interventions
+    #' @param int_1 string representing the first intervention
+    #' @param int_2 string representing the second intervention (intechangable
+    #' with int_1)
+    #' @param rho value between -1 and 1 representing the correlation between rounds of
+    #' the intervention
+    inter_intervention_rho = function(int_1, int_2, rho) {
+      if (!(int_1 %in% private$interventions)) {
+        stop(paste0('invalid intervention name: ', int))
+      }
+      if (!(int_2 %in% private$interventions)) {
+        stop(paste0('invalid intervention name: ', int))
+      }
+      if (rho < -1 || rho > 1) {
+        stop(paste0(
+          'rho between ',
+          int_1,
+          ' and ',
+          int_2,
+          'must be between -1 and 1'
+        ))
+      }
+      private$rho_matrix[[int_1, int_2]] <- rho
+      private$rho_matrix[[int_2, int_1]] <- rho
+      private$.sigma <- NULL
+    },
+
+    #' @description Standard deviation of each intervention between rounds
+    sigma = function() {
+      if (is.null(private$.sigma)) {
+        rho <- private$rho()
+        private$.sigma <- sqrt(rho / (1 - rho))
+        names(private$.sigma) <- private$interventions
+      }
+      private$.sigma
+    },
+
+    #' @description multivariate norm draws for these parameters
+    mvnorm = function() {
+      if (is.null(private$.mvnorm)) {
+        rho <- private$rho()
+        sigma <- self$sigma()
+        V <- outer(sigma, sigma) * rho
+        diag(V) <- sigma ^ 2
+        private$.mvnorm <- MASS::mvrnorm(
+          private$population,
+          rep(0, length(private$interventions)),
+          V
+        )
+        dimnames(private$.mvnorm)[[2]] <- private$interventions
+      }
+      private$.mvnorm
+    }
+  )
+)
+
+get_correlation_parameters <- function(parameters) {
+  CorrelationParameters$new(parameters)
+}
+
+sample_intervention <- function(intervention, n, p, c_param) {
+  sigma_squared <- c_param$sigma()[[intervention]]^2
+  sd <- sqrt(1 + sigma_squared)
+  u0 <- -qnorm(p, 0) * sd
+  z <- rnorm(n)
+  u0 + c_param$mvnorm()[,intervention] + z < 0
+}
---
+++ tests/testthat/test-correlation.R
@@ -0,0 +1,100 @@
+test_that('1 correlation between rounds gives sensible samples', {
+  pop <- 1e6
+  parameters <- get_parameters(list(
+    human_population = pop,
+    rtss = TRUE,
+    rtss_coverage = .2
+  ))
+  c_param <- get_correlation_parameters(parameters)
+  c_param$inter_round_rho('rtss', 1)
+  round_1 <- sample_intervention('rtss', pop, parameters$rtss_coverage, c_param)
+  round_2 <- sample_intervention('rtss', pop, parameters$rtss_coverage, c_param)
+  expect_equal(sum(round_1), pop * .2, tolerance=1e2)
+  expect_equal(sum(round_2), pop * .2, tolerance=1e2)
+  expect_equal(sum(round_1 & round_2), pop * .2, tolerance=1e2)
+})
+
+test_that('0 correlation between rounds gives sensible samples', {
+  pop <- 1e6
+  parameters <- get_parameters(list(
+    human_population = pop,
+    rtss = TRUE,
+    rtss_coverage = .5
+  ))
+  c_param <- get_correlation_parameters(parameters)
+  c_param$inter_round_rho('rtss', 0)
+  round_1 <- sample_intervention('rtss', pop, parameters$rtss_coverage, c_param)
+  round_2 <- sample_intervention('rtss', pop, parameters$rtss_coverage, c_param)
+  expect_equal(
+    length(intersect(which(round_1), which(round_2))),
+    pop * .5,
+    tolerance=1e2
+  )
+  expect_equal(sum(round_1), sum(round_2), tolerance=1e2)
+  expect_equal(sum(round_1), pop * .5, tolerance=1e2)
+})
+
+test_that('1 correlation between interventions gives sensible samples', {
+  pop <- 1e6
+  parameters <- get_parameters(list(
+    human_population = pop,
+    rtss_coverage = .2,
+    mda_coverage = .2,
+    rtss = TRUE,
+    mda = TRUE
+  ))
+  c_param <- get_correlation_parameters(parameters)
+  c_param$inter_round_rho('rtss', 1)
+  c_param$inter_round_rho('mda', 1)
+  c_param$inter_intervention_rho('rtss', 'mda', 1)
+  rtss_sample <- sample_intervention('rtss', pop, parameters$rtss_coverage, c_param)
+  mda_sample <- sample_intervention('mda', pop, parameters$mda_coverage, c_param)
+
+  expect_equal(sum(rtss_sample), pop * .2, tolerance=1e2)
+  expect_equal(sum(mda_sample), pop * .2, tolerance=1e2)
+  expect_equal(sum(rtss_sample & mda_sample), pop * .2, tolerance=1e2)
+})
+
+test_that('0 correlation between interventions gives sensible samples', {
+  pop <- 1e6
+  parameters <- get_parameters(list(
+    human_population = pop,
+    rtss_coverage = .2,
+    mda_coverage = .2,
+    rtss = TRUE,
+    mda = TRUE
+  ))
+  c_param <- get_correlation_parameters(parameters)
+  c_param$inter_round_rho('rtss', 1)
+  c_param$inter_round_rho('mda', 1)
+  c_param$inter_intervention_rho('rtss', 'mda', 0)
+  rtss_sample <- sample_intervention('rtss', pop, parameters$rtss_coverage, c_param)
+  mda_sample <- sample_intervention('mda', pop, parameters$mda_coverage, c_param)
+  expect_equal(
+    length(intersect(which(rtss_sample), which(mda_sample))),
+    pop * .5,
+    tolerance=1e2
+  )
+  expect_equal(sum(rtss_sample), sum(mda_sample), tolerance=1e2)
+  expect_equal(sum(rtss_sample), pop * .5, tolerance=1e2)
+})
+
+test_that('-1 correlation between interventions gives sensible samples', {
+  pop <- 1e6
+  parameters <- get_parameters(list(
+    human_population = pop,
+    rtss_coverage = .2,
+    mda_coverage = .2,
+    rtss = TRUE,
+    mda = TRUE
+  ))
+  c_param <- get_correlation_parameters(parameters)
+  c_param$inter_round_rho('rtss', 1)
+  c_param$inter_round_rho('mda', 1)
+  c_param$inter_intervention_rho('rtss', 'mda', -1)
+  rtss_sample <- sample_intervention('rtss', pop, parameters$rtss_coverage, c_param)
+  mda_sample <- sample_intervention('mda', pop, parameters$mda_coverage, c_param)
+  expect_equal(length(intersect(which(rtss_sample), which(mda_sample))), 0)
+  expect_equal(sum(rtss_sample), .2 * pop, tolerance=1e2)
+  expect_equal(sum(mda_sample), .2 * pop, tolerance=1e2)
+})

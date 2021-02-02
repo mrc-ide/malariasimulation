cec3b0cac57b74f52a53bbb917470e1f2b3ceb27
Giovanni Charles <giovanni.charles@gmail.com>
2021-02-02 17:35:12 +0000

    Get RTS,S to pass

--- R/rtss.R
+++ R/rtss.R
@@ -3,25 +3,21 @@
 #' strategy from `set_rtss` and correlation parameters from
 #' `get_correlation_parameters`
 #'
-#' @param human the handle for the human individual
 #' @param variables list of variables in the model
 #' @param events a list of events in the model
 #' @param parameters the model parameters
 #' @param correlations correlation parameters
+#' @param renderer rendering object
 create_rtss_vaccination_listener <- function(
-  human,
   variables,
   events,
   parameters,
-  correlations
+  correlations,
+  renderer
   ) {
-  function(api, target) {
-    timestep <- api$get_timestep()
-    age <- get_age(
-      api$get_variable(human, variables$birth),
-      timestep
-    )
-    not_vaccinated <- api$get_variable(human, variables$rtss_vaccinated) == -1
+  function(timestep) {
+    age <- get_age(variables$birth$get_values(), timestep)
+    not_vaccinated <- variables$rtss_vaccinated$get_values() == -1
     target_indices <- rep(FALSE, length(age))
     for (i in seq_along(parameters$rtss_min_ages)) {
       target_indices <- target_indices | (
@@ -31,23 +27,25 @@ create_rtss_vaccination_listener <- function(
     }
     target <- which(target_indices & not_vaccinated)
     target <- target[
-      sample_intervention(target, 'rtss', parameters$rtss_coverage, correlations)
+      sample_intervention(
+        target,
+        'rtss',
+        parameters$rtss_coverage,
+        correlations
+      )
     ]
-    api$render('n_vaccinated', length(target))
+    renderer$render('n_vaccinated', length(target), timestep)
     if (length(target) > 0) {
-      api$queue_variable_update(
-        human,
-        variables$rtss_vaccinated,
+      variables$rtss_vaccinated$queue_update(
         timestep,
         target
       )
     }
     if (timestep + parameters$rtss_frequency <= parameters$rtss_end) {
-      api$schedule(events$rtss_vaccination, c(1), parameters$rtss_frequency)
+      events$rtss_vaccination$schedule(parameters$rtss_frequency)
     }
     if (length(parameters$rtss_boosters) > 0) {
-      api$schedule(
-        events$rtss_booster,
+      events$rtss_booster$schedule(
         target[bernoulli(length(target), parameters$rtss_booster_coverage[[1]])],
         parameters$rtss_boosters[[1]]
       )
@@ -55,35 +53,28 @@ create_rtss_vaccination_listener <- function(
   }
 }
 
-create_rtss_booster_listener <- function(human, variables, events, parameters) {
-  function(api, target) {
-		api$queue_variable_update(
-			human,
-			variables$rtss_cs,
+create_rtss_booster_listener <- function(variables, events, parameters) {
+  function(timestep, target) {
+    variables$rtss_cs$queue_update(
 			exp(
-        parameters$rtss_cs_boost[[1]] + parameters$rtss_cs_boost[[2]] * rnorm(length(target))
+        parameters$rtss_cs_boost[[1]] + parameters$rtss_cs_boost[[2]] * rnorm(target$size())
       ),
       target
 		)
 
-		api$queue_variable_update(
-			human,
-			variables$rtss_rho,
+    variables$rtss_rho$queue_update(
 			invlogit(
-        parameters$rtss_rho_boost[[1]] + parameters$rtss_rho_boost[[2]] * rnorm(length(target))
+        parameters$rtss_rho_boost[[1]] + parameters$rtss_rho_boost[[2]] * rnorm(target$size())
       ),
 			target
 		)
 
-    timestep <- api$get_timestep()
-    api$queue_variable_update(
-			human,
-			variables$rtss_boosted,
+    variables$rtss_boosted$queue_update(
       timestep,
 			target
 		)
 
-    vaccinated <- api$get_variable(human, variables$rtss_vaccinated, target)
+    vaccinated <- variables$rtss_vaccinated$get_values(target)
     for (v in unique(vaccinated)) {
       for (i in seq_len(length(parameters$rtss_boosters) - 1)) {
         to_boost <- which(
@@ -91,11 +82,11 @@ create_rtss_booster_listener <- function(human, variables, events, parameters) {
         )
 
         if (length(to_boost) > 0) {
-          to_boost <- to_boost[
-            bernoulli(length(to_boost), parameters$rtss_booster_coverage[[i + 1]])
-          ]
-          api$schedule(
-            events$rtss_booster,
+          to_boost <- to_boost[bernoulli(
+            length(to_boost),
+            parameters$rtss_booster_coverage[[i + 1]]
+          )]
+          events$rtss_booster$schedule(
             to_boost,
             v + parameters$rtss_boosters[[i + 1]] - timestep
           )

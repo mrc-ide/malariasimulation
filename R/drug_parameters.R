b9903568a1e7f2cae17dd07f7359323feaba2d19
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2021-05-21 01:16:32 -0700

    adding drug names

--- R/drug_parameters.R
+++ R/drug_parameters.R
@@ -1,19 +1,19 @@
 #' @title Preset parameters for the DHA-PQP drug
 #' @description From SI of Commun. 5:5606 doi: 10.1038/ncomms6606 (2014)
-#' @details Use a vector of preset parameters for the DHA-PQP drug
+#' @details Use a vector of preset parameters for the DHA-PQP drug (dihydroartemisinin-piperaquine)
 #' @details Default parameters, from L to R, are: drug_efficacy: 0.95, drug_rel_c: 0.09434, drug_prophylaxis_shape: 4.4, drug_prophylaxis_scale: 28.1
 #' @export
 DHA_PQP_params <- c(.95, 0.09434, 4.4, 28.1)
 
 #' @title Preset parameters for the AL drug
 #' @description From SI of Commun. 5:5606 doi: 10.1038/ncomms6606 (2014)
-#' @details Use a vector of preset parameters for the AL drug
+#' @details Use a vector of preset parameters for the AL drug (artemether-lumefantrine)
 #' @details Default parameters, from L to R, are: drug_efficacy: 0.95, drug_rel_c: 0.05094, drug_prophylaxis_shape: 11.3, drug_prophylaxis_scale: 10.6
 #' @export
 AL_params <- c(.95, 0.05094, 11.3, 10.6)
 
 #' @title Preset parameters for the SP-AQ drug
-#' @details Use a vector of preset parameters for the SP-AQ drug
+#' @details Use a vector of preset parameters for the SP-AQ drug (sulphadoxine-pyrimethamine and amodiaquine)
 #' @details Default parameters, from L to R, are: drug_efficacy: 0.9, drug_rel_c: 0.32, drug_prophylaxis_shape: 4.3, drug_prophylaxis_scale: 38.1
 #' @export
 SP_AQ_params <- c(0.9, 0.32, 4.3, 38.1)

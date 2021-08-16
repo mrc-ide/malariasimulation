a7f6e53703e8a5c54dbeab0d3ac1daf2a49c2356
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2021-08-16 10:15:04 +0100

    Revert "adding vars for num and den"
    This reverts commit 58710b8750fb82aabd2d5834327534dc31c66111.

--- R/render.R
+++ R/render.R
@@ -1,3 +1,16 @@
+epi_from_subpopulation <- function(target, age, lower, upper) {
+  in_range <- individual::Bitset$new(target$max_size)$insert(
+    which((age >= lower) & (age <= upper))
+  )
+
+  n_in_range <- in_range$size()
+  if (n_in_range == 0) {
+    return(0)
+  }
+  in_range$and(target)$size() / n_in_range
+}
+
+## NEW
 count_of_subpopulation <- function(target, age, lower, upper) {
   in_range <- individual::Bitset$new(target$max_size)$insert(
     which((age >= lower) & (age <= upper))
@@ -9,6 +22,7 @@ count_of_subpopulation <- function(target, age, lower, upper) {
   n_in_range
 }
 
+## NEW
 count_of_detected <- function(target, age, lower, upper) {
   in_range <- individual::Bitset$new(target$max_size)$insert(
     which((age >= lower) & (age <= upper))
@@ -20,9 +34,6 @@ count_of_detected <- function(target, age, lower, upper) {
   in_range$and(target)$size() 
 }
 
-# calculate # of people in age group, 
-# number detected by microscopy (all), 
-# number of severe (all)
 create_prevelance_renderer <- function(
   state,
   birth,
@@ -42,6 +53,7 @@ create_prevelance_renderer <- function(
         parameters
       )
     )
+
     detected <- state$get_index_of(c('Tr', 'D'))$or(
       bitset_at(asymptomatic, asymptomatic_detected)
     )
@@ -50,37 +62,42 @@ create_prevelance_renderer <- function(
     for (i in seq_along(parameters$prevalence_rendering_min_ages)) {
       lower <- parameters$prevalence_rendering_min_ages[[i]]
       upper <- parameters$prevalence_rendering_max_ages[[i]]
-      p <- count_of_subpopulation( 
+      p <- epi_from_subpopulation(
         detected,
         age,
         lower,
         upper
       )
-      renderer$render(paste0('n_', lower, '_', upper), p, timestep) 
-      p <- count_of_detected( 
+      renderer$render(paste0('pv_', lower, '_', upper), p, timestep)      
+      q <- count_of_subpopulation( #NEW
         detected,
         age,
         lower,
         upper
       )
-      renderer$render(paste0('n_', 'detected_', lower, '_', upper), p, timestep) # NEW
+      renderer$render(paste0('n_', lower, '_', upper), q, timestep) # NEW
+      r <- count_of_detected( #NEW
+        detected,
+        age,
+        lower,
+        upper
+      )
+      renderer$render(paste0('n_', 'detected_', lower, '_', upper), r, timestep) # NEW
     }
     for (i in seq_along(parameters$severe_prevalence_rendering_min_ages)) {
       lower <- parameters$severe_prevalence_rendering_min_ages[[i]]
       upper <- parameters$severe_prevalence_rendering_max_ages[[i]]
-      p <- count_of_detected(
+      p <- epi_from_subpopulation(
         severe,
         age,
         lower,
         upper
       )
-      renderer$render(paste0('n_', 'severe_', lower, '_', upper), p, timestep)
+      renderer$render(paste0('pv_severe_', lower, '_', upper), p, timestep)
     }
   }
 }
 
-# calculate number detected by microscopy (new in that timestep), 
-# number of severe (new in that timestep)
 create_incidence_renderer <- function(birth, is_severe, parameters, renderer) {
   function(timestep, target) {
     age <- get_age(birth$get_values(), timestep)
@@ -88,24 +105,39 @@ create_incidence_renderer <- function(birth, is_severe, parameters, renderer) {
     for (i in seq_along(parameters$incidence_rendering_min_ages)) {
       lower <- parameters$incidence_rendering_min_ages[[i]]
       upper <- parameters$incidence_rendering_max_ages[[i]]
-      p <- count_of_detected( 
+      p <- epi_from_subpopulation(
         target,
         age,
         lower,
         upper
       )
-      renderer$render(paste0('n_', 'incident_', lower, '_', upper), p, timestep) 
+      renderer$render(paste0('inc_', lower, '_', upper), p, timestep)       
+      q <- count_of_subpopulation( #NEW
+        detected,
+        age,
+        lower,
+        upper
+      )
+     
+      renderer$render(paste0('n_', lower, '_', upper), q, timestep) # NEW
+      r <- count_of_detected( #NEW
+        detected,
+        age,
+        lower,
+        upper
+      )
+      renderer$render(paste0('n_', 'detected_', lower, '_', upper), r, timestep) # NEW
     }
     for (i in seq_along(parameters$severe_incidence_rendering_min_ages)) {
       lower <- parameters$severe_incidence_rendering_min_ages[[i]]
       upper <- parameters$severe_incidence_rendering_max_ages[[i]]
-      p <- count_of_detected(
+      p <- epi_from_subpopulation(
         severe$copy()$and(target),
         age,
         lower,
         upper
       )
-      renderer$render(paste0('n_', 'incident_', 'severe_', lower, '_', upper), p, timestep)
+      renderer$render(paste0('inc_severe_', lower, '_', upper), p, timestep)
     }
   }
 }
@@ -116,13 +148,27 @@ create_clinical_incidence_renderer <- function(birth, parameters, renderer) {
     for (i in seq_along(parameters$clinical_incidence_rendering_min_ages)) {
       lower <- parameters$clinical_incidence_rendering_min_ages[[i]]
       upper <- parameters$clinical_incidence_rendering_max_ages[[i]]
-      p <- count_of_detected( 
+      p <- epi_from_subpopulation(
         target,
         age,
         lower,
         upper
       )
-      renderer$render(paste0('n_', 'incident_', 'clinical_', lower, '_', upper), p, timestep) 
+      renderer$render(paste0('clin_inc_', lower, '_', upper), p, timestep)       
+      q <- count_of_subpopulation( #NEW
+        detected,
+        age,
+        lower,
+        upper
+      )
+      renderer$render(paste0('n_', lower, '_', upper), q, timestep) # NEW      
+      r <- count_of_detected( #NEW
+        detected,
+        age,
+        lower,
+        upper
+      )
+      renderer$render(paste0('n_', 'detected_', lower, '_', upper), r, timestep) # NEW
     }
   }
 }
--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -11,7 +11,7 @@ test_that('that default rendering works', {
   immunity <- individual::DoubleVariable$new(rep(1, 4))
   is_severe <- individual::CategoricalVariable$new(
     c('yes', 'no'),
-    c('no', 'no', 'yes', 'no')
+    rep('no', 4)
   )
 
 
@@ -29,21 +29,12 @@ test_that('that default rendering works', {
   process(timestep)
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     1,
-    'n_730_3650',
-    3,
+    'pv_730_3650',
+    2/3,
     timestep
   )
-  
-  mockery::expect_args(
-    renderer$render_mock(),
-    2,
-    'n_detected_730_3650',
-    2,
-    timestep
-  )
-
 })
 
 test_that('that default rendering works when no one is in the age range', {
@@ -75,9 +66,9 @@ test_that('that default rendering works when no one is in the age range', {
   mockery::stub(process, 'bernoulli_multi_p', mockery::mock(1))
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     1,
-    'n_730_3650',
+    'pv_730_3650',
     0,
     timestep
   )
@@ -116,18 +107,18 @@ test_that('that severe rendering works', {
   process(timestep)
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     1,
-    'n_severe_0_1825',
-    1,
+    'pv_severe_0_1825',
+    1/2,
     timestep
   )
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     2,
-    'n_severe_730_3650',
-    1,
+    'pv_severe_730_3650',
+    1/3,
     timestep
   )
 })
@@ -151,19 +142,19 @@ test_that('that clinical incidence rendering works', {
 
   process(timestep, individual::Bitset$new(4)$insert(c(1, 2, 4)))
 
-  mockery::expect_args( 
-    renderer$render_mock(),
+  mockery::expect_args(
+    renderer$render,
     1,
-    'n_incident_clinical_0_1825',
-    2,
+    'clin_inc_0_1825',
+    1,
     timestep
   )
-  
-  mockery::expect_args( 
-    renderer$render_mock(),
+
+  mockery::expect_args(
+    renderer$render,
     2,
-    'n_incident_clinical_730_3650',
-    2,
+    'clin_inc_730_3650',
+    2/3,
     timestep
   )
 })
@@ -194,18 +185,18 @@ test_that('that incidence rendering works', {
   process(timestep, individual::Bitset$new(4)$insert(c(1, 2, 4)))
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     1,
-    'n_incident_0_1825',
-    2,
+    'inc_0_1825',
+    1,
     timestep
   )
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     2,
-    'n_incident_730_3650',
-    2,
+    'inc_730_3650',
+    2/3,
     timestep
   )
 })
@@ -235,18 +226,18 @@ test_that('that severe incidence rendering works', {
   process(timestep, individual::Bitset$new(4)$insert(c(1, 2, 4)))
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     1,
-    'n_incident_severe_0_1825',
+    'inc_severe_0_1825',
     0,
     timestep
   )
 
   mockery::expect_args(
-    renderer$render_mock(),
+    renderer$render,
     2,
-    'n_incident_severe_730_3650',
-    1,
+    'inc_severe_730_3650',
+    1/3,
     timestep
   )
 })

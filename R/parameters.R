22ddaa621e318763ff939b4af8a184aaebde40d1
Giovanni Charles <giovanni.charles@gmail.com>
2022-12-19 15:32:41 +0000

    Update vaccine parameterisation

--- R/parameters.R
+++ R/parameters.R
@@ -164,23 +164,14 @@
 #' clinically diseased (these values refer to the index in drug_* parameters); default = NULL, NULL, NULL
 #' * clinical_treatment_coverage - a vector of coverage values for each drug; default = NULL, NULL, NULL
 #'
-#' RTS,S paramters:
-#' please set RTS,S strategies with the convenience functions in
-#' `vaccine_parameters.R:set_rtss_epi`
-#' `vaccine_parameters.R:set_mass_rtss`
+#' PEV parameters: 
+#' please set vaccine strategies with the convenience functions in
+#' `pev_parameters.R:set_vaccine_epi`
+#' `pev_parameters.R:set_mass_vaccine`
 #'
-#' * rtss_doses - the dosing schedule before the vaccine takes effect; default =
+#' * pev_doses - the dosing schedule before the vaccine takes effect; default =
 #' c(0, 1.5 * 30, 3 * 30)
 #' default = 365
-#' * rtss_vmax - the maximum efficacy of the vaccine; default = 0.93
-#' * rtss_alpha - shape parameter for the vaccine efficacy model; default = 0.74
-#' * rtss_beta - scale parameter for the vaccine efficacy model; default = 99.4
-#' * rtss_cs - peak parameters for the antibody model (mean and std. dev); default = 6.37008, 0.35
-#' * rtss_cs_boost - peak parameters for the antibody model for booster rounds (mean and std. dev); default = 5.56277, 0.35
-#' * rtss_rho - delay parameters for the antibody model (mean and std. dev); default = 2.37832, 1.00813
-#' * rtss_rho_boost - delay parameters for the antibody model for booster rounds (mean and std. dev); default = 1.03431, 1.02735
-#' * rtss_ds - delay parameters for the antibody model, short-term weaning (mean and std. dev); default = 3.74502, 0.341185 (White MT et al. 2015 Lancet ID)
-#' * rtss_dl - delay parameters for the antibody model, long-term weaning (mean and std. dev); default = 6.30365, 0.396515 (White MT et al. 2015 Lancet ID)
 #'
 #' MDA, SMC and PMC parameters:
 #' please set these parameters with the convenience functions in `mda_parameters.R`
@@ -348,17 +339,8 @@ get_parameters <- function(overrides = list()) {
     clinical_treatment_timesteps = list(),
     clinical_treatment_coverages = list(),
     # rts,s
-    rtss           = FALSE,
-    rtss_doses     = c(0, 1.5 * 30, 3 * 30),
-    rtss_vmax      = .93,
-    rtss_alpha     = .74,
-    rtss_beta      = 99.4,
-    rtss_cs        = c(6.37008, 0.35),
-    rtss_cs_boost  = c(5.56277, 0.35),
-    rtss_rho       = c(2.37832, 1.00813),
-    rtss_rho_boost = c(1.03431, 1.02735),
-    rtss_ds        = c(3.74502, 0.341185),
-    rtss_dl        = c(6.30365, 0.396515),
+    vaccines       = FALSE,
+    vaccine_doses  = c(0, 1.5 * 30, 3 * 30),
     # MDA
     mda = FALSE,
     mda_drug = 0,
--- R/vaccine_parameters.R
+++ R/vaccine_parameters.R
@@ -1,37 +1,118 @@
-#' @title Parameterise an RTS,S epi strategy
+#' @title Vaccine profile
+#' @description A data structure for holding vaccine profile parameters.
+#' Parameters are validated on creation.
+#' @importFrom R6 R6Class
+#' @export
+VaccineProfile <- R6::R6Class(
+  'VaccineProfile',
+  public = list(
+    #' @field vmax maximum efficacy of the vaccine
+    vmax,
+    #' @field alpha shape parameter for the vaccine efficacy model
+    alpha,
+    #' @field beta scale parameter for the vaccine efficacy model
+    beta,
+    #' @field cs peak parameters for the antibody model (vector of mean and std. dev)
+    cs,
+    #' @field rho delay parameters for the antibody model (vector of mean and std. dev)
+    rho,
+    #' @field ds delay parameters for the antibody model, short-term waning (vector of mean and std. dev)
+    ds,
+    #' @field dl delay parameters for the antibody model, long-term waning (vector of mean and std. dev)
+    dl,
+
+    #' @description create a vaccine profile
+    #' @param vmax immutable value for vmax
+    #' @param alpha immutable value for alpha
+    #' @param beta immutable value for beta
+    #' @param cs immutable value for cs
+    #' @param rho immutable value for rho
+    #' @param ds immutable value for ds
+    #' @param dl immutable value for dl
+    initialize = function(vmax, alpha, beta, cs, rho, ds, dl) {
+      allargs <- c(vmax, alpha, beta, cs, rho, ds, dl)
+      stopifnot(all(is.numeric(allargs)))
+      stopifnot(all(allargs > 0))
+      stopifnot(length(cs) == 2)
+      stopifnot(length(rho) == 2)
+      stopifnot(length(ds) == 2)
+      stopifnot(length(dl) == 2)
+      self$vmax = vmax
+      self$alpha = alpha
+      self$beta = beta
+      self$cs = cs
+      self$rho = rho
+      self$ds = ds
+      self$dl = dl
+    }
+  )
+)
+
+#' @export
+rtss_profile <- VaccineProfile$new(
+  vmax = 0.93
+  alpha = 0.74
+  beta = 99.4
+  cs = c(6.37008, 0.35)
+  rho = c(2.37832, 1.00813)
+  ds = c(3.74502, 0.341185) # (White MT et al. 2015 Lancet ID)
+  dl = c(6.30365, 0.396515) # (White MT et al. 2015 Lancet ID)
+)
+
+#' @export
+rtss_booster_profile <- VaccineProfile$new(
+  vmax = 0.93
+  alpha = 0.74
+  beta = 99.4
+  cs = c(5.56277, 0.35)
+  rho = c(1.03431, 1.02735)
+  ds = c(3.74502, 0.341185) # (White MT et al. 2015 Lancet ID)
+  dl = c(6.30365, 0.396515) # (White MT et al. 2015 Lancet ID)
+)
+
+#' @title Parameterise a vaccine epi strategy
 #'
-#' @description distribute RTS,S vaccine when an individual becomes a certain
+#' @description distribute vaccine when an individual becomes a certain
 #' age. Efficacy will take effect after the last dose
 #'
 #' @param parameters a list of parameters to modify
-#' @param coverages a vector of coverages for the starter doses
+#' @param profile primary vaccine profile of type VaccineProfile
+#' @param coverages a vector of coverages for the primary doses
 #' @param timesteps a vector of timesteps associated with coverages
 #' @param age for the target population, (in timesteps)
 #' @param min_wait the minimum acceptable time since the last vaccination (in
 #' timesteps); When seasonal_boosters = TRUE, this represents the minimum time
 #' between an individual receiving the final dose and the first booster. When using
-#' both set_mass_rtss and set_rtss_epi, this represents the minimum time between
+#' both set_mass_vaccine and set_vaccine_epi, this represents the minimum time between
 #' an individual being vaccinated under one scheme and vaccinated under another.
-#' @param boosters the timesteps (following the final dose) at which booster vaccinations are administered
+#' @param booster_timestep the timesteps (following the final dose) at which booster vaccinations are administered
 #' @param booster_coverage the proportion of the vaccinated population who will
 #' receive each booster vaccine
+#' @param booster_profile list of booster vaccine profiles, of type
+#' VaccineProfile, for each timestep in booster_timeteps
 #' @param seasonal_boosters logical, if TRUE the first booster timestep is
 #' relative to the start of the year, otherwise they are relative to the last dose
 #' @export
-set_rtss_epi <- function(
+set_vaccine_epi <- function(
   parameters,
+  profile,
   coverages,
   timesteps,
   age,
   min_wait,
-  boosters,
+  booster_timestep,
   booster_coverage,
+  booster_profile,
   seasonal_boosters = FALSE
   ) {
   stopifnot(all(coverages >= 0) && all(coverages <= 1))
+
+  # Check that the primary timing parameters make sense
   if(length(coverages) != length(timesteps)){
     stop("coverages and timesteps must align")
   }
+
+  # Check that seasonal booster parameters make sense
   stopifnot(min_wait >= 0)
   stopifnot(age >= 0)
   stopifnot(is.logical(seasonal_boosters))
@@ -40,28 +121,40 @@ set_rtss_epi <- function(
       boosters <- boosters + 365
     }
   }
-  stopifnot((length(boosters) == 0) || all(boosters > 0))
+
+  # Check that the booster timing parameters make sense
+  stopifnot((length(booster_timestep) == 0) || all(boosters > 0))
   stopifnot((length(booster_coverage)) == 0 || all(booster_coverage >= 0 & booster_coverage <= 1))
-  if (length(booster_coverage) != length(boosters)) {
-    stop('booster and booster_coverage does not align')
+  if (!all(c(length(booster_coverage), length(booster_timestep), length(booster_profile)) == length(booster_timestep))) {
+    stop('booster_timestep and booster_coverage and booster_profile does not align')
   }
-  parameters$rtss <- TRUE
-  parameters$rtss_epi_coverages <- coverages
-  parameters$rtss_epi_timesteps <- timesteps
-  parameters$rtss_epi_age <- age
-  parameters$rtss_epi_boosters <- boosters
-  parameters$rtss_epi_min_wait <- min_wait
-  parameters$rtss_epi_booster_coverage <- booster_coverage
-  parameters$rtss_epi_seasonal_boosters <- seasonal_boosters
+
+  # Check that vaccine profiles are well formed
+  stopifnot(inherits(profile, 'VaccineProfile'))
+  for (p in booster_profile) {
+    stopifnot(inherits(p, 'VaccineProfile'))
+  }
+
+  parameters$vaccines <- TRUE
+  parameters$vaccine_epi_profile <- profile
+  parameters$vaccine_epi_coverages <- coverages
+  parameters$vaccine_epi_timesteps <- timesteps
+  parameters$vaccine_epi_age <- age
+  parameters$vaccine_epi_booster_timestep <- booster_timestep
+  parameters$vaccine_epi_min_wait <- min_wait
+  parameters$vaccine_epi_booster_coverage <- booster_coverage
+  parameters$vaccine_epi_booster_profile <- booster_profile
+  parameters$vaccine_epi_seasonal_boosters <- seasonal_boosters
   parameters
 }
 
-#' @title Parameterise an RTS,S mass distribution strategy
+#' @title Parameterise a vaccine mass distribution strategy
 #'
-#' @description distribute RTS,S vaccine to a population in an age range.
+#' @description distribute vaccine to a population in an age range.
 #' Efficacy will take effect after the last dose
 #'
 #' @param parameters a list of parameters to modify
+#' @param profile primary vaccine profile of type VaccineProfile
 #' @param timesteps a vector of timesteps for each round of vaccinations
 #' @param coverages the coverage for each round of vaccinations
 #' @param min_wait the minimum acceptable time since the last vaccination (in timesteps);
@@ -69,40 +162,51 @@ set_rtss_epi <- function(
 #' time between an individual being vaccinated under one scheme and vaccinated under another.
 #' @param min_ages for the target population, inclusive (in timesteps)
 #' @param max_ages for the target population, inclusive (in timesteps)
-#' @param boosters the timesteps (following the initial vaccination) at which booster vaccinations are administered
+#' @param booster_timestep the timesteps (following the initial vaccination) at which booster vaccinations are administered
 #' @param booster_coverage the proportion of the vaccinated population who will
 #' receive each booster vaccine
+#' @param booster_profile list of booster vaccine profiles, of type
+#' VaccineProfile, for each timestep in booster_timeteps
 #' @export
-set_mass_rtss <- function(
+set_mass_vaccination <- function(
   parameters,
+  profile,
   timesteps,
   coverages,
   min_ages,
   max_ages,
   min_wait,
-  boosters,
+  booster_timestep,
   booster_coverage
+  booster_profile,
   ) {
   stopifnot(all(timesteps > 1))
   stopifnot(min_wait >= 0)
   stopifnot(all(coverages >= 0) && all(coverages <= 1))
   stopifnot(all(min_ages >= 0 & max_ages >= 0))
-  stopifnot(all(boosters > 0))
+  stopifnot(all(booster_timestep > 0))
   stopifnot(all(booster_coverage >= 0 & booster_coverage <= 1))
   if (length(min_ages) != length(max_ages)) {
     stop('min and max ages do not align')
   }
-  if (length(booster_coverage) != length(boosters)) {
-    stop('booster and booster_coverage does not align')
+  if (!all(c(length(booster_coverage), length(booster_timestep), length(booster_profile)) == length(booster_timestep))) {
+    stop('booster_timestep, booster_coverage and booster_profile does not align')
   }
-  parameters$rtss <- TRUE
-  parameters$rtss_mass_timesteps <- timesteps
-  parameters$rtss_mass_coverages <- coverages
-  parameters$rtss_mass_min_ages <- min_ages
-  parameters$rtss_mass_max_ages <- max_ages
-  parameters$rtss_mass_min_wait <- min_wait
-  parameters$rtss_mass_boosters <- boosters
-  parameters$rtss_mass_booster_coverage <- booster_coverage
+
+  # Check that vaccine profiles are well formed
+  stopifnot(inherits(profile, 'VaccineProfile'))
+  for (p in booster_profile) {
+    stopifnot(inherits(p, 'VaccineProfile'))
+  }
+  parameters$vaccines <- TRUE
+  parameters$mass_vaccine_timesteps <- timesteps
+  parameters$mass_vaccine_coverages <- coverages
+  parameters$mass_vaccine_min_ages <- min_ages
+  parameters$mass_vaccine_max_ages <- max_ages
+  parameters$mass_vaccine_min_wait <- min_wait
+  parameters$mass_vaccine_booster_timestep <- booster_timestep
+  parameters$mass_vaccine_booster_coverage <- booster_coverage
+  parameters$mass_vaccine_booster_profile <- booster_profile
   parameters
 }
 

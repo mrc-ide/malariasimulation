d262b7b97f258c98cc53eaed9663c9f5f4b772ae
Peter Winskill <p.winskill@imperial.ac.uk>
2023-02-15 14:21:57 +0000

    Tidy draw tests

--- tests/testthat/test-parameter_draws.R
+++ tests/testthat/test-parameter_draws.R
@@ -2,6 +2,10 @@ test_that("Out of range error", {
   p <- get_parameters()
   expect_error(set_parameter_draw(p, 0), "draw must be an integer between 1 and 1000")
   expect_error(set_parameter_draw(p, 1001), "draw must be an integer between 1 and 1000")
+})
+
+test_that("Draw overwrite works", {
+  p <- get_parameters()
   p1 <- set_parameter_draw(p, 1)
   expect_identical(p1[names(parameter_draws[[1]])], parameter_draws[[1]])
   p2 <- set_parameter_draw(p, 1000)

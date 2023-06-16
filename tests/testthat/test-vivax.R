c1de4790489eb8d740482e3bef491191d798befb
RJSheppard <rjs11@ic.ac.uk>
2023-06-16 12:52:40 +0100

    Corrected mistake in test, function added to namespace

--- tests/testthat/test-vivax.R
+++ tests/testthat/test-vivax.R
@@ -24,6 +24,7 @@ test_that('Test age structure should not change vivax infectivity', {
   )
   
   # Generate different age structure
+  year <- 365
   ages <- round(c(0.083333, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45,
                   50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 200) * year)
   

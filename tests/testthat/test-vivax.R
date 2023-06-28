4a51fc15c60b72164b1745c7a6275f5be83a27ef
RJSheppard <rjs11@ic.ac.uk>
2023-06-28 11:17:56 +0100

    Fixing tests.

--- tests/testthat/test-vivax.R
+++ tests/testthat/test-vivax.R
@@ -1,14 +1,28 @@
-test_that('Test falciparum model runs', {
-  parameters <- get_parameters()
-  sim_res <- run_simulation(timesteps = 100, parameters = parameters)
+test_that('Test falciparum switch produces same', {
+  parameters_def <- get_parameters()
+  parameters_fal <- get_parameters(parasite = "falciparum")
+  expect_identical(parameters_def, parameters_fal)
+})
+
+test_that('Test vivax model runs', {
+  vivax_parameters <- get_parameters(parasite = "vivax")
+  sim_res <- run_simulation(timesteps = 100, parameters = vivax_parameters)
   expect_equal(nrow(sim_res), 100)
 })
 
-test_that('Test model vivax runs', {
-  parameters <- get_parameters()
-  vivax_parameters <- set_parasite(parameters = parameters, parasite = "vivax")
-  sim_res <- run_simulation(timesteps = 100, parameters = parameters)
-  expect_equal(nrow(sim_res), 100)
+test_that('Test parasite = vivax sets parameters$parasite to vivax', {
+  vivax_parameters <- get_parameters(parasite = "vivax")
+  expect_identical(vivax_parameters$parasite, "vivax")
+})
+
+test_that('Test difference between falciparum and vivax parameter lists', {
+  falciparum_parameters <- get_parameters(parasite = "falciparum")
+  vivax_parameters <- get_parameters(parasite = "vivax")
+  
+  expect_true(all(names(falciparum_parameters)[!names(falciparum_parameters) %in% names(vivax_parameters)] %in%
+                    c("du","rvm","rva","rb","b0","b1","ib0","kb","theta0","theta1","kv","fv0","av","gammav","iv0","fd0","ad","gammad","d1","id0","kd","ub","uv","gamma1","pvm","init_ivm","init_ib","init_iva")))
+  expect_true(all(names(vivax_parameters[!names(vivax_parameters) %in% names(falciparum_parameters)]) %in%
+                    c("du_max","du_min","ku","au50","b","phi0lm","phi1lm","ic0lm","kclm","d0","ca","init_idm","f","gammal")))
 })
 
 test_that('Test age structure should not change vivax infectivity', {

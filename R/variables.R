a2f7b9a2afeda068bc862663064757affeeff26a
RJSheppard <rjs11@ic.ac.uk>
2023-09-27 15:36:25 +0100

    Improved readability. Added parts to allow the model to run (initial hypnozoite allocation).

--- R/variables.R
+++ R/variables.R
@@ -20,7 +20,7 @@
 #' * zeta_group - Discretised heterogeneity of human individuals
 #' * pev_timestep - The timestep of the last pev vaccination (-1 if there
 #' haven't been any)
-#' * pev_profile - The index of the profile of the last administered pev vaccine 
+#' * pev_profile - The index of the profile of the last administered pev vaccine
 #' (-1 if there haven't been any)
 #' * tbv_vaccinated - The timstep of the last tbv vaccination (-1 if there
 #' haven't been any
@@ -30,7 +30,7 @@
 #' * drug - The last prescribed drug
 #' * drug_time - The timestep of the last drug
 #'
-#' Mosquito variables are: 
+#' Mosquito variables are:
 #' * mosquito_state - the state of the mosquito, a category Sm|Pm|Im|NonExistent
 #' * species - the species of mosquito, this is a category gamb|fun|arab
 #'
@@ -92,7 +92,7 @@ create_variables <- function(parameters) {
   birth <- individual::IntegerVariable$new(-initial_age)
   last_boosted_ica <- individual::DoubleVariable$new(rep(-1, size))
   last_boosted_id <- individual::DoubleVariable$new(rep(-1, size))
-  
+
   # Maternal immunity to clinical disease
   icm <- individual::DoubleVariable$new(
     initial_immunity(
@@ -104,7 +104,7 @@ create_variables <- function(parameters) {
       'ICM'
     )
   )
-  
+
   # Acquired immunity to clinical disease
   ica <- individual::DoubleVariable$new(
     initial_immunity(
@@ -116,7 +116,7 @@ create_variables <- function(parameters) {
       'ICA'
     )
   )
-  
+
   # Acquired immunity to detectability
   id <- individual::DoubleVariable$new(
     initial_immunity(
@@ -128,14 +128,14 @@ create_variables <- function(parameters) {
       'ID'
     )
   )
-  
+
   # Severe disease and pre-ertythrocitic (blood) immunity only modelled in P. falciparum
   if(parameters$parasite == "falciparum"){
-    
+
     # Boost immunities
     last_boosted_ib <- individual::DoubleVariable$new(rep(-1, size))
     last_boosted_iva <- individual::DoubleVariable$new(rep(-1, size))
-    
+
     # Maternal severe disease immunity
     ivm <- individual::DoubleVariable$new(
       initial_immunity(
@@ -147,7 +147,7 @@ create_variables <- function(parameters) {
         'IVM'
       )
     )
-    
+
     # Acquired immunity to severe disease
     iva <- individual::DoubleVariable$new(
       initial_immunity(
@@ -159,7 +159,7 @@ create_variables <- function(parameters) {
         'IVA'
       )
     )
-    
+
     # Pre-erythoctic immunity
     ib  <- individual::DoubleVariable$new(
       initial_immunity(
@@ -171,7 +171,7 @@ create_variables <- function(parameters) {
         'IB'
       )
     )
-    
+
   } else if (parameters$parasite == "vivax"){
     # Maternal immunity to detectable disease only modelled in P. vivax
     idm <- individual::DoubleVariable$new(
@@ -184,10 +184,16 @@ create_variables <- function(parameters) {
         'IDM'
       )
     )
-    
+
     # Hypnozoite batch variable
     hypnozoites <- individual::IntegerVariable$new(
-      rep(parameters$init_hyp, size))
+      initial_hypnozoites(
+        parameters$init_hyp,
+        initial_age,
+        groups,
+        eq
+      )
+    )
   }
 
   # Initialise infectiousness of humans -> mosquitoes
@@ -246,9 +252,9 @@ create_variables <- function(parameters) {
     net_time = net_time,
     spray_time = spray_time
   )
-  
+
   if(parameters$parasite == "falciparum"){
-    variables <- c(variables, 
+    variables <- c(variables,
                    last_boosted_ib = last_boosted_ib,
                    last_boosted_iva = last_boosted_iva,
                    ivm = ivm,
@@ -256,8 +262,8 @@ create_variables <- function(parameters) {
                    iva = iva
     )
   } else if(parameters$parasite == "vivax"){
-    variables <- c(variables, 
-                   idm = idm, 
+    variables <- c(variables,
+                   idm = idm,
                    hypnozoites = hypnozoites)
   }
 
@@ -374,6 +380,21 @@ initial_state <- function(parameters, age, groups, eq, states) {
     ))
   }
   rep(ibm_states, times = calculate_initial_counts(parameters))
+}
+
+initial_hypnozoites <- function(parameter, age, groups, eq){
+  if (!is.null(eq)) {
+    age <- age / 365
+    return(vnapply(
+      seq_along(age),
+      function(i) {
+        g <- groups[[i]]
+        a <- age[[i]]
+        rpois(n = 1, lambda = eq[[g]][which.max(a < eq[[g]][, 'age']), "HH"])
+      }
+    ))
+  }
+  rep(parameter, length(age))
 }
 
 calculate_initial_counts <- function(parameters) {

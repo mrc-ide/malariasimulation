72d2849edd54099754c5d1b1121e6fe45c7f8297
NoSchID <n.schmit17@imperial.ac.uk>
2024-04-23 16:58:30 +0100

    Typo

--- R/pev_parameters.R
+++ R/pev_parameters.R
@@ -91,7 +91,7 @@ vivax_blood_stage_profile <- list(
 #' @description Parameters for a booster dose of a blood-stage  vivax vaccine for use with the
 #' set_mass_pev and set_pev_epi functions
 #' @export
-vivax_blood_stage_profile <- list(
+vivax_blood_stage_booster_profile <- list(
   v0_patent = 0.6, 
   vhl_patent = 3*365
 )

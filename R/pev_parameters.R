730f83a5001588a69d74362c24faa5b2d883be5b
NoSchID <n.schmit17@imperial.ac.uk>
2024-04-25 16:24:40 +0100

    Reset blood-stage efficacy

--- R/pev_parameters.R
+++ R/pev_parameters.R
@@ -83,7 +83,7 @@ vivax_pev_booster_profile <- list(
 #' set_mass_pev and set_pev_epi functions
 #' @export
 vivax_blood_stage_profile <- list(
-  v0_patent = 0.9, 
+  v0_patent = 0.6, 
   vhl_patent = 3*365
 )
 
@@ -92,7 +92,7 @@ vivax_blood_stage_profile <- list(
 #' set_mass_pev and set_pev_epi functions
 #' @export
 vivax_blood_stage_booster_profile <- list(
-  v0_patent = 0.9, 
+  v0_patent = 0.6, 
   vhl_patent = 3*365
 )
 

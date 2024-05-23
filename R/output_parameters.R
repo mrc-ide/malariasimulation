e004d5b150e9b12c709d96bf37462361b5343deb
RJSheppard <rjs11@ic.ac.uk>
2024-05-23 12:51:40 +0100

    Changed age limits back to continuous groups (no discrete age groups will be rendered now). Made the code more easier to read.

--- R/output_parameters.R
+++ R/output_parameters.R
@@ -1,8 +1,8 @@
 #' @title Parameterise age grouped output rendering
 #'
-#' @details this function produces discrete and contiguous age groups, inclusive of the lower
-#' age limit and exclusive of the upper age limit: e.g., list(c(0, 10, 100), c(200, 250) will produce
-#' three age groups: 0-9, 10-99 and 200-249 in days.
+#' @details this function produces contiguous age groups, inclusive of the lower
+#' age limit and exclusive of the upper age limit: e.g., c(0, 10, 100) will produce
+#' two age groups: 0-9 and 10-99 in days.
 #' @param parameters the model parameters
 #' @param age_group age breaks for population size outputs; default = NULL
 #' @param incidence age breaks for incidence outputs (D+Tr+A); default = NULL
@@ -31,15 +31,29 @@ set_epi_outputs <- function(parameters,
                             ib = NULL
 ){
   
-  parent_formals <- names(formals())
-  parent_formals <- parent_formals[which(parent_formals != "parameters")]
-  outputs <- parent_formals[!unlist(lapply(parent_formals, function(x){is.null(get(x))}))]
+  input <- list(
+    age_group = age_group,
+    incidence = incidence,
+    clinical_incidence = clinical_incidence,
+    severe_incidence = severe_incidence,
+    prevalence = prevalence,
+    ica = ica,
+    icm = icm,
+    iva = iva,
+    ivm = ivm,
+    id = id,
+    ib = ib
+  )
+  input <- input[!sapply(input, is.null)]
   
-  for (output in outputs) {
-    if(!is.list(get(output))){stop("Each age input must be a list of vectors")}
-    parameters[[paste0(output, "_rendering_min_ages")]] <- unlist(lapply(get(output), function(x){x[-length(x)]}))
-    parameters[[paste0(output, "_rendering_max_ages")]] <- unlist(lapply(get(output), function(x){x[-1]-1}))
+  for(i in seq_along(input)){
+    name <- names(input)[i]
+    ages <- input[[i]]
+    min_ages <- ages[-length(ages)]
+    max_ages <- ages[-1] - 1
+    parameters[[paste0(name, "_rendering_min_ages")]] <- min_ages
+    parameters[[paste0(name, "_rendering_max_ages")]] <- max_ages
   }
-  
-  parameters
+
+  return(parameters)
 }
--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -2,17 +2,17 @@ test_that('Test age parameter function works', {
   parameters <- get_parameters()
   age_limits <- c(0,1,2,3)*365
   parameters <- set_epi_outputs(parameters,
-                                age_group = list(age_limits),
-                                incidence = list(age_limits+1),
-                                clinical_incidence = list(age_limits+2),
-                                severe_incidence = list(age_limits+3),
-                                prevalence = list(age_limits+4),
-                                ica = list(age_limits+5),
-                                icm = list(age_limits+6),
-                                id = list(age_limits+7),
-                                ib = list(age_limits+8),
-                                iva = list(age_limits+9),
-                                ivm = list(age_limits+10)
+                                age_group = age_limits,
+                                incidence = age_limits+1,
+                                clinical_incidence = age_limits+2,
+                                severe_incidence = age_limits+3,
+                                prevalence = age_limits+4,
+                                ica = age_limits+5,
+                                icm = age_limits+6,
+                                id = age_limits+7,
+                                ib = age_limits+8,
+                                iva = age_limits+9,
+                                ivm = age_limits+10
   )
   
   sim <- run_simulation(timesteps = 1, parameters)
@@ -26,23 +26,6 @@ test_that('Test age parameter function works', {
                    "n","n_detect","p_detect",
                    "ica_mean", "icm_mean","id_mean","ib_mean","iva_mean","ivm_mean"), each = 3),"_",
              age_limits[-4]+rep(c(0,rep(c(1:4), each = 3),5:10), each = 3),"_",age_limits[-1]-1+rep(c(0,rep(c(1:4), each = 3),5:10), each = 3)
-      ) %in%
-        names(sim)))
-  
-})
-
-test_that('Test age parameter multiple sequences function works', {
-  parameters <- get_parameters()
-  age_limits <- c(0,1,2,3)*365
-  parameters <- set_epi_outputs(parameters,
-                                age_group = list(age_limits, age_limits+100))
-  
-  sim <- run_simulation(timesteps = 1, parameters)
-  
-  expect_true(
-    all(
-      paste0(rep(c("n_age"), each = 3),"_",
-             c(age_limits[-4], age_limits[-4]+100), "_", c(age_limits[-1], age_limits[-1]+100)-1
       ) %in%
         names(sim)))
   

c3dc720b3a4ff977137815a0f8b809e52446713e
RJSheppard <rjs11@ic.ac.uk>
2024-06-28 11:43:46 +0100

    Added parameters csv file that contains falciparum and vivax specific parameter sets. These parameter values are associated with the set that fixes b = 0.25 and sigma_squared = 1.67, as recommended by Michael White.
    These parameters are selected in the get_parameters() function by specifying parasite == "falciparum" or "vivax". The functions then selects the parasite-specific values and combines then withe the non-parasite specific values.
    
    In order to allow the vivax code to run without full functionality, we have included additional falciparum parameters in the vivax parameter set. These will be removed in the following stages.
    
    Pv parameter draws have been added, with code to read them into the package.
    
    Added tests to check parameters are behaving as expected.
    
    A new vignette has been started to reflect the P. vivax additions that have been made.

--- tests/testthat/test-parameter_draws.R
+++ tests/testthat/test-parameter_draws.R
@@ -7,7 +7,21 @@ test_that("Out of range error", {
 test_that("Draw overwrite works", {
   p <- get_parameters()
   p1 <- set_parameter_draw(p, 1)
-  expect_identical(p1[names(parameter_draws[[1]])], parameter_draws[[1]])
+  expect_identical(p1[names(parameter_draws_pf[[1]])], parameter_draws_pf[[1]])
   p2 <- set_parameter_draw(p, 1000)
-  expect_identical(p2[names(parameter_draws[[1000]])], parameter_draws[[1000]])
+  expect_identical(p2[names(parameter_draws_pf[[1000]])], parameter_draws_pf[[1000]])
+})
+
+test_that("Out of range error (vivax)", {
+  p <- get_parameters(parasite = "vivax")
+  expect_error(set_parameter_draw(p, 0), "draw must be an integer between 1 and 1000")
+  expect_error(set_parameter_draw(p, 1001), "draw must be an integer between 1 and 1000")
+})
+
+test_that("Draw overwrite works (vivax)", {
+  p <- get_parameters(parasite = "vivax")
+  p1 <- set_parameter_draw(p, 1)
+  expect_identical(p1[names(parameter_draws_pv[[1]])], parameter_draws_pv[[1]])
+  p2 <- set_parameter_draw(p, 1000)
+  expect_identical(p2[names(parameter_draws_pv[[1000]])], parameter_draws_pv[[1000]])
 })
---
+++ tests/testthat/test-vivax.R
@@ -0,0 +1,40 @@
+test_that('Test falciparum switch produces same', {
+  parameters_def <- get_parameters()
+  parameters_fal <- get_parameters(parasite = "falciparum")
+  expect_identical(parameters_def, parameters_fal)
+})
+
+test_that('Test vivax model runs', {
+  vivax_parameters <- get_parameters(parasite = "vivax")
+  sim_res <- run_simulation(timesteps = 100, parameters = vivax_parameters)
+  expect_equal(nrow(sim_res), 100)
+})
+
+test_that('Test parasite = vivax sets parameters$parasite to vivax', {
+  vivax_parameters <- get_parameters(parasite = "vivax")
+  expect_identical(vivax_parameters$parasite, "vivax")
+})
+
+test_that('Test difference between falciparum and vivax parameter lists', {
+  falciparum_parameters <- get_parameters(parasite = "falciparum")
+  vivax_parameters <- get_parameters(parasite = "vivax")
+  
+  in_falciparum_not_vivax <- setdiff(names(falciparum_parameters), names(vivax_parameters))
+  in_vivax_not_falciparum <- setdiff(names(vivax_parameters), names(falciparum_parameters))
+  
+  expect_identical(
+    in_falciparum_not_vivax,
+    character(0)
+  )
+  
+  expect_identical(
+    in_vivax_not_falciparum,
+    c("dpcr_max", "dpcr_min", "kpcr", "apcr50", # human sub-patent state delay
+      "init_iaa", "init_iam", "ra", "ua", # antiparasite immunity parameters)
+      "b", # probability of infection given an infectious bite
+      "philm_min", "philm_max", "klm", "alm50", # probability of light-microscopy detectable infection parameters
+      "ca", # light-microscopy detectable infection infectivity towards mosquitos
+      "f", "gammal", "init_hyp", "kmax" # hypnozoite parameters
+    )
+  )
+})

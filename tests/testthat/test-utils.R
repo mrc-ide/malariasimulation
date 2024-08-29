4913f03d264eee22d241d431710bc2692619e225
Paul Lietar <pl2113@ic.ac.uk>
2024-08-29 12:23:17 +0100

    Add a range check to bitset_index.
    The function assumes the two bitsets are the same size, but was never
    checking for it. If the user passed in a second bitset that is smaller
    than the first, it could lead to an out-of-bounds access on the
    underlying std::vector.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -21,3 +21,23 @@ test_that('time_cached calls again for a different timestep', {
   expect_equal(cached_fn(timestep = 2), 43)
   mockery::expect_called(fn, 2)
 })
+
+test_that("bitset_index works", {
+  a <- individual::Bitset$new(10)$insert(c(3,5,7,9))
+  b <- individual::Bitset$new(10)$insert(c(2,4,5,8,9))
+  expect_equal(bitset_index(a, b), c(2,4))
+
+  a <- individual::Bitset$new(10)
+  b <- individual::Bitset$new(10)$insert(c(2,4,5,8,9))
+  expect_equal(length(bitset_index(a, b)), 0)
+
+  a <- individual::Bitset$new(10)$insert(c(3,5,7,9))
+  b <- individual::Bitset$new(10)
+  expect_equal(length(bitset_index(a, b)), 0)
+})
+
+test_that("bitset_index errors if size does not match", {
+  a <- individual::Bitset$new(10)$insert(c(3,5,7,9))
+  b <- individual::Bitset$new(20)$insert(c(2,4,5,8,9))
+  expect_error(bitset_index(a, b), "Incompatible bitmap sizes")
+})

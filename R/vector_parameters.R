85304c40c3fdcf9d4ffa5c142b6cc9142efe747f
EllieSherrardSmith <EllieSherrardSmith@users.noreply.github.com>
2024-09-12 10:34:28 +1000

    Update vector_parameters.R
    adding semiochemical_effect for altering blood feeding rate at select timestep

--- R/vector_parameters.R
+++ R/vector_parameters.R
@@ -6,6 +6,7 @@
 #' Q0: 0.92
 #' phi_bednets: 0.85
 #' phi_indoors: 0.90
+#' semiochemical_effect: 1
 #' mum: 0.132
 #'
 #' parameters from:
@@ -18,6 +19,7 @@ gamb_params <- list(
   Q0 = .92,
   phi_bednets = .85,
   phi_indoors = .90,
+  semiochemical_effect = 1,
   mum = .132
 )
 
@@ -29,6 +31,7 @@ gamb_params <- list(
 #' Q0: 0.71
 #' phi_bednets: 0.8
 #' phi_indoors: 0.86
+#' semiochemical_effect: 1
 #' mum: 0.132
 #'
 #' parameters from:
@@ -41,6 +44,7 @@ arab_params <- list(
   Q0 = .71,
   phi_bednets = .8,
   phi_indoors = .86,
+  semiochemical_effect = 1,
   mum = .132
 )
 
@@ -52,6 +56,7 @@ arab_params <- list(
 #' Q0: 0.94
 #' phi_bednets: 0.78
 #' phi_indoors: 0.87
+#' semiochemical: 1
 #' mum: 0.112
 #'
 #' parameters from:
@@ -64,6 +69,7 @@ fun_params <- list(
   Q0 = .94,
   phi_bednets = .78,
   phi_indoors = .87,
+  semiochemical_effect = 1,
   mum = .112
 )
 
@@ -75,6 +81,7 @@ fun_params <- list(
 #' Q0: 0.21
 #' phi_bednets: 0.57
 #' phi_indoors: 0.37
+#' semiochemical_effect: 1
 #' mum: 0.112
 #'
 #' parameters reference:
@@ -91,6 +98,7 @@ steph_params <- list(
   Q0 = 0.21,
   phi_bednets = 0.52186,
   phi_indoors = 0.4776,
+  semiochemical_effect = 1,
   mum = .112
 )
 
@@ -114,17 +122,18 @@ set_species <- function(parameters, species, proportions) {
     'Q0',
     'phi_bednets',
     'phi_indoors',
+    'semiochemical_effect',
     'mum'
   )
   for (key in keys) {
     parameters[[key]] <- rep(NA, length(species))
   }
   for (v in seq_along(species)) {
-    if (species[[v]]$foraging_time > 1 / species[[v]]$blood_meal_rates) {
+    if (species[[v]]$foraging_time > 1 / (species[[v]]$blood_meal_rates * species[[v]]$semiochemical_effect) ) {
       stop(
         sprintf(
           "blood meal time (%f) must be >= foraging time (%f)",
-          1 / species[[v]]$blood_meal_rates,
+          1 / (species[[v]]$blood_meal_rates * species[[v]]$semiochemical_effect),
           species[[v]]$foraging_time
         )
       )

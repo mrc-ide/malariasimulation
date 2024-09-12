1fa8f7896fd1521be5b40ea42220477b1e30c9ab
EllieSherrardSmith <EllieSherrardSmith@users.noreply.github.com>
2024-09-13 07:40:58 +1000

    Update parameters.R
    creating the list for semiochemicals

--- R/parameters.R
+++ R/parameters.R
@@ -150,8 +150,8 @@
 #' * spraying - boolean for if indoor spraying is enabled; default = FALSE
 #' * phi_indoors - proportion of bites taken indoors; default = 0.90
 #' * semiochemical - boolean for if semiochemical exposure is enabled; default = FALSE
-#' * semiochemical_effect - the proportional reduction introduced on blood feeding rates for each species; default = 1
-#' * semiochemical_timesteps - to enable a reduced foraging time to be implemented as required; default = NULL
+#' * semiochemical_effect - the proportional reduction introduced on blood feeding rates for each species; default = NULL
+#' * chemical_timesteps - to enable a reduced foraging time to be implemented as required; default = NULL
 #'
 #' treatment parameters:
 #' please set treatment parameters with the convenience functions in
@@ -335,7 +335,7 @@ get_parameters <- function(overrides = list()) {
     phi_indoors = .90,
     # semiochemical use
     semiochemical = FALSE,
-    semiochemical_effect = 1,
+    semiochemical_effect = list(),
     semiochemical_timesteps = list(),
     # treatment
     drug_efficacy          = numeric(0),

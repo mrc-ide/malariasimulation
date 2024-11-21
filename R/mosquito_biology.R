a59d99a1e5fb9f4cd493088317c4066fc9bf5312
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2024-11-21 13:43:29 +0000

    mum is part of parameters, so don't need to call it

--- R/mosquito_biology.R
+++ R/mosquito_biology.R
@@ -149,7 +149,7 @@ peak_season_offset <- function(parameters) {
 #' @noRd
 death_rate <- function(f, W, Z, species, parameters,timestep) {
   mum <- parameters$mum[[species]]
-  mum <- endec_adjusted_mortality(mum, parameters, species, timestep)
+  mum <- endec_adjusted_mortality(parameters, species, timestep)
   p1_0 <- exp(-mum * parameters$foraging_time[[species]])
   gonotrophic_cycle <- get_gonotrophic_cycle(species, parameters)
   p2 <- exp(-mum * gonotrophic_cycle) #change what goes into background mort

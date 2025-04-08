ce285586cc331e537eb7dd0ade68a997e3609473
NoSchID <43637540+NoSchID@users.noreply.github.com>
2025-04-08 14:57:13 +0100

    Update set_epi_outputs

--- R/output_parameters.R
+++ R/output_parameters.R
@@ -57,16 +57,16 @@ set_epi_outputs <- function(parameters,
     hypnozoites = hypnozoites
   )
   
-  if(parameters$parasite == "falciparum"){
-    if(!any(lapply(list(n_with_hypnozoites, iaa, iam, hypnozoites), is.null))){
-      stop("Some selected epi outputs do not apply to this parasite.")
-    }
-    
-  } else if (parameters$parasite == "vivax"){
-    if(!any(lapply(list(severe_incidence, iva, ivm, id, ib), is.null))){
-      stop("Some selected epi outputs do not apply to this parasite.")
-    }
-  }
+  # if(parameters$parasite == "falciparum"){
+  #   if(!any(lapply(list(n_with_hypnozoites, iaa, iam, hypnozoites), is.null))){
+  #     stop("Some selected epi outputs do not apply to this parasite.")
+  #   }
+  #   
+  # } else if (parameters$parasite == "vivax"){
+  #   if(!any(lapply(list(severe_incidence, iva, ivm, id, ib), is.null))){
+  #     stop("Some selected epi outputs do not apply to this parasite.")
+  #   }
+  # }
   
   input <- input[!sapply(input, is.null)]
   
